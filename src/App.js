0f355c0b4174c88f84bed1ff09a2277113e10a85
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-09-29 21:49:29 +0300

    feat: create App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+import Form from './components/Form.js';
+import FormButton from './components/FormButton.js';
+import AdminPanel from './components/AdminPanel.js';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="app">
+      <h1 className="sr-only">Admin Panel</h1>
+      <div className="box">
+        <Form heading="Sign up" />
+        <Form heading="Log in" />
+      </div>
+      <div className="btn-group">
+        <FormButton text="Sign up"/>
+        <FormButton text="Log in"/>
+      </div>
+      <AdminPanel hidden={true} />
+    </section>
   );
 }
 
